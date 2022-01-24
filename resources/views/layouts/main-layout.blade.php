cc5365e3311ccca5f66aa15af60c19440ab952db
MyUsername <my@email.com>
2022-01-24 16:05:13 +0100

    add: npm

--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -4,11 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Hello World!</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <title>Comics</title>
 </head>
 <body>
     <section>
         @include('components.header')
+
+        @include('components.jumbotron')
 
         @yield('content')
 
@@ -16,42 +20,3 @@
     </section>
 </body>
 </html>
-
-<style>
-    * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
-
-    header {
-        height: 100px;
-        background-color: aqua;
-        text-align: center;
-    }
-
-    #home h1 {
-        height: 50px;
-        background-color: brown;
-        text-align: center;
-    }
-
-    #home img {
-        display: block;
-        border-radius: 10px;
-    }
-
-    #game h1 {
-        height: 50px;
-        background-color: brown;
-        text-align: center;
-    }
-
-    footer {
-        height: 100px;
-        margin-top: 100px;
-        background-color: blue;
-        color: white;
-        text-align: center;
-    }
-</style>
